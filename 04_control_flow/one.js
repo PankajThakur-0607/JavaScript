c2d1b67a40579975ede0b076e02c4401a77cf8fd
Pankaj Thakur <109215106+PankajThakur-0607@users.noreply.github.com>
2024-06-17 15:09:28 +0530

    Add files via upload

---
+++ 04_control_flow/one.js
@@ -0,0 +1,25 @@
+// if
+    isUserLoggedIn = true;
+    let temp = 41;
+if (temp < 50) {
+    console.log("less than 50");
+}else{
+    console.log("temp is greater");
+}
+
+
+const score = 200 
+if(score < 500) console.log("hello") , console.log("world");;
+// if(score > 100){
+//     // let power = "fly";
+//     var power = "fly"
+//     console.log(`user power : ${power}`);
+// }
+
+
+// console.log(`user power : ${power}`);
+
+const bal = 1000;
+// if(bal > 50) console.log("test1");
+
+
